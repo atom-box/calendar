b242294ef50e8db61b64fccfde98594c78ced5e8
Evan Genest <genest.1@osu.edu>
2017-08-09 17:26:24 -0500

    Goofin was made from my memory.  Button clicks.  JSON next.

---
+++ aug9/goofinRote.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function(event)
+{
+	document.querySelector("button").addEventListener
+	(
+		"click", 
+		function
+		()
+		{
+			// Your legit shiznit goes here.
+			console.log("Red rubber baby bumpers");			
+		}
+		
+	)
+});
+
+// If this works it'll be sort of empowering.
+
+// WORKED!  With 14 minutes de-bugging and
+// one Dad gift purchased.
