da9e44cc6dfc1e34becd72c50642053d134a441b
Evan Genest <genest.1@osu.edu>
2017-08-31 17:11:41 -0500

    Remove upper case typo in file name.

---
+++ aug31/script.js
@@ -0,0 +1,8 @@
+/*
+TODO Aug 30[30 mins and then quit]
+1 --Add proper heading to THIS JS
+2 -- bootstrap not loading?  bootstrap not centering or responding . . .
+3-- the forms and buttons.
+*/
+
+console.log("Spike");
